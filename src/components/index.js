b50c54c48ba0a763753579beb27e125ecf745427
Harshitsriv007 <harshit.srivastv726@gmail.com>
2022-06-30 01:09:55 +0530

    master

--- src/components/index.js
+++ src/components/index.js
@@ -8,7 +8,6 @@ export default function Index()
 {
     const [data,setData]= useState([]);
     const [text,setText]= useState([]);
-    // const [deleteData,setDeleteData]= useState([]);
     const [count,setCount]= useState(0);
     const [id,setId]= useState(0);
 
